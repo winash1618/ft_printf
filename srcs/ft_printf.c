2d6512d75e5b952e204099b1795f509f460d64fa
asharaf <51042907+winash1618@users.noreply.github.com>
2022-02-27 08:05:39 +0400

    Update printf.c

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkaruvan <mkaruvan@sstudent.42abudhabi.ae  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/27 03:25:23 by mkaruvan          #+#    #+#             */
+/*   Updated: 2022/02/27 03:25:36 by mkaruvan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
-int ft_char(const char c, va_list ap)
+int ft_charcheck(const char c, va_list ap)
 {
     int len;
     
@@ -10,10 +22,18 @@ int ft_char(const char c, va_list ap)
     else if (c == 's')
         len += ft_putstr(va_args(ap, char *));
     else if (c == 'p')
-        len += ft_putptr(va_args(ap, unsigned long long));
+        len += ft_putaddr(va_args(ap, unsigned long long));
     else if (c == 'd' || c = 'i')
-        len += ft
+        len += ft_putint(va_args(ap, int));
+    else if (c == 'u')
+        len += ft_putunint(va_args(ap, unsigned int));
+    else if (c == 'x' || c == 'X')
+        len += ft_puthex(va_args(ap, unsigned int));
+    else if (c == '%')
+        len += ft_putchar(c);
+    return (len);
 }
+
 int	ft_printf(const char *str, ...)
 {
     va_list ap;
@@ -25,13 +45,9 @@ int	ft_printf(const char *str, ...)
     while (str[i])
     {
         if (str[i] != '%')
-        {
             len += ft_putchar(str[i]);
-        }
         else
-        {
-            len += ft_char(str[i + 1], ap);
-        }
+            len += ft_charcheck(str[++i], ap);
         i++;
     }
     va_end(ap);
